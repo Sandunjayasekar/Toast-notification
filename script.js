14d8b4e9d592a5581fa5b7fca3fc648373ba1dc6
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2024-06-13 14:19:12 +0530

    h1 heading added

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ let errorMsg = '<i class="fa-solid fa-xmark"></i>Something went wrong';
 let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invalid input, Check again';
 
 function showToast(msg){
-    console.log(msg);
+
     let toast = document.createElement('div');
     toast.classList.add('toast');
     toast.innerHTML = msg;
     toastBox.appendChild(toast);
+
     setTimeout(() => {
         toast.remove();
     }, 3000);
